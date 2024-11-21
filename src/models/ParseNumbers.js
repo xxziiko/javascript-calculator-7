a510997f2eb8128507b5e2ff6c78f42e4bed8430
xxziiko <xxziiko@gmail.com>
2024-11-21 15:33:03 +0900

    feat(ParseNumbers): 분리한 양수 문자열을 숫자로 변환 후 유효성 검사

--- src/models/ParseNumbers.js
+++ src/models/ParseNumbers.js
@@ -9,14 +9,28 @@ class ParseNumbers {
     this.#CUSTOM_START = '//';
     this.#delimiter = [',', ':'];
     this.#validateDelimiter(inputs);
+    this.#numbers = this.#parseNumbers(inputs);
+    this.#validateNumbers(this.#numbers);
   }
 
+  get numbers() {
+    return this.#numbers;
+  }
 
   #parseNumbers(inputs) {
-    if (!inputs.startsWith(this.#CUSTOM_START)) return this.#parseStringToArray(inputs);
+    const strings = this.#parseInputs(inputs);
+    return this.#parseStringToNumbers(strings);
+  }
+
+  #parseStringToNumbers(string) {
+    return string.map(Number);
+  }
+
+  #parseInputs(inputs) {
+    if (!inputs.startsWith(this.#CUSTOM_START)) return this.#parseStringArray(inputs);
 
     const numberString = this.#processCustomDelimiter(inputs);
-    return this.#parseStringToArray(numberString);
+    return this.#parseStringArray(numberString);
   }
 
   #processCustomDelimiter(inputs) {
@@ -29,11 +43,15 @@ class ParseNumbers {
     return inputs.slice(findIndex + TARGET_STRING.length);
   }
 
-  #parseStringToArray(string) {
+  #parseStringArray(string) {
     const regex = new RegExp(`[${this.#delimiter.join('')}]`);
     return string.split(regex);
   }
 
+  #validateNumbers(numbers) {
+    Validations.validateNegativeNumbers(numbers);
+    Validations.validateIncludeNonNumbers(numbers);
+  }
 
   #validateDelimiter(inputs) {
     Validations.validateDelimiter(inputs);
--- src/shared/Validations.js
+++ src/shared/Validations.js
@@ -2,6 +2,15 @@ import { DEFAULT_DELIMITERS, ERROR } from './constants.js';
 import Utils from './Utils.js';
 
 class Validations {
+  static validateNegativeNumbers(numbers) {
+    const isNegative = numbers.some((number) => number < 0);
+    Utils.handleError(isNegative, ERROR.invalidNumber);
+  }
+
+  static validateIncludeNonNumbers(numbers) {
+    const isIncludeNonNumbers = numbers.some((number) => Number.isNaN(number));
+    Utils.handleError(isIncludeNonNumbers, ERROR.includeNonNumbers);
+  }
 
   static validateDelimiter(inputs) {
     const hasDefaultDelimiter = DEFAULT_DELIMITERS.some((delimiter) => inputs.includes(delimiter));
