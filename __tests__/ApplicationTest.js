0325a2e07e75bceffb39c7bf97734490bb419e77
xxziiko <xxziiko@gmail.com>
2024-11-21 15:06:21 +0900

    test: 기본 구분자 사용, 예외 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,43 +1,97 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { EOL as LINE_SEPARATOR } from 'os';
 
 const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
+  const messages = [];
 
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+  MissionUtils.Console.readLineAsync = jest.fn((prompt) => {
+    messages.push(prompt);
     const input = inputs.shift();
+
+    if (input === undefined) {
+      throw new Error('NO INPUT');
+    }
+
     return Promise.resolve(input);
   });
+
+  MissionUtils.Console.readLineAsync.messages = messages;
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("문자열 계산기", () => {
-  test("커스텀 구분자 사용", async () => {
-    const inputs = ["//;\\n1"];
-    mockQuestions(inputs);
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join(LINE_SEPARATOR);
+};
 
-    const logSpy = getLogSpy();
-    const outputs = ["결과 : 1"];
+const expectLogContains = (received, expects) => {
+  expects.forEach((exp) => {
+    expect(received).toContain(exp);
+  });
+};
 
-    const app = new App();
-    await app.run();
+const runExceptions = async ({
+  inputs = [],
+  inputsToTerminate = [],
+  expectedErrorMessage = '',
+}) => {
+  // given
+  const logSpy = getLogSpy();
+  mockQuestions([...inputs, ...inputsToTerminate]);
 
-    outputs.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  // when
+  const app = new App();
+  await app.run();
+
+  // then
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expectedErrorMessage));
+};
+
+const run = async ({ inputs = [], inputsToTerminate = [], expected = [] }) => {
+  // given
+  const logSpy = getLogSpy();
+  mockQuestions([...inputs, ...inputsToTerminate]);
+
+  // when
+  const app = new App();
+  await app.run();
+
+  const output = getOutput(logSpy);
+
+  // then
+
+  if (expected.length > 0) {
+    expectLogContains(output, expected);
+  }
+};
+
+describe('문자열 계산기', () => {
+  test('커스텀 구분자 사용', async () => {
+    await run({
+      inputs: ['//;\n1;2;3'],
+      inputsToTerminate: ['//;\n1;2;3'],
+      expected: ['결과 : 6'],
     });
   });
 
-  test("예외 테스트", async () => {
-    const inputs = ["-1,2,3"];
-    mockQuestions(inputs);
+  test('기본 구분자 사용', async () => {
+    await run({
+      inputs: ['1,2:3'],
+      inputsToTerminate: ['1,2:3'],
+      expected: ['결과 : 6'],
+    });
+  });
 
-    const app = new App();
-
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+  test.each([['-1,2,3'], ['a,1,2'], ['//;\n1'], ['1234567890']])('예외 테스트', async (input) => {
+    await runExceptions({
+      inputs: [input],
+      expectedErrorMessage: '[ERROR]',
+      inputsToTerminate: ['1,2,3'],
+    });
   });
 });
